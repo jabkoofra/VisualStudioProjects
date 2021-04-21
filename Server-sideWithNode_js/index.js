686dd5c4c8c1bf331b95a21b1c555a43efe95393
jabkoofra <przemyslaw.jablonski92@gmail.com>
2021-04-21 16:43:57 +0200

    update

--- Server-sideWithNode_js/index.js
+++ Server-sideWithNode_js/index.js
@@ -11,11 +11,26 @@ app.use(express.json({
 }));
 
 const database = new Datastore('database.db');
-database.loadDatabase(); 
+database.loadDatabase();
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+       if(err){
+           response.end()
+           return;
+       }
+        response.json(data);
+    })
+})
 
 app.post('/api', (request, response) => {
     console.log('I got request: ')
     console.log(request.body);
-    
-    response.json();
+
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+
+    response.json(data);
 })
