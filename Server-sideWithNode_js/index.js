ec9fd60d8d4a8318eb2e6665e82898161d7188ea
jabkoofra <przemyslaw.jablonski92@gmail.com>
2021-04-21 13:31:13 +0200

    installSerwer

---
+++ Server-sideWithNode_js/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+
+app.listen(3000,
+    ()=> console.log('listening at 3000'));
+
+app.use(express.static('public'));
+app.post('/api', (request, response)=>{
+    console.log('I got request: ')
+    console.log(request.body);
+})
